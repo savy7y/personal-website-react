803ef9997df6823d0316166c3a357435ce92ef96
divy <93451471+savy7y@users.noreply.github.com>
2023-04-12 21:38:49 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { LogoLink } from '../components/logo/LogoLink';
+
 import { Content } from '../components/content/Content';
 import { Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import DisplacementSphere from '../components/background/DisplacementSphere';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
-import { FooterText } from '../components/footer/FooterText';
+
 import { SocialIcons } from '../components/content/SocialIcons';
 import { SpeedDials } from '../components/speedDial/SpeedDial';
 
